ef23fdbb80a2eff3608115e67962f2c0fd91e3b0
zhaza <Khail.ballin12@yahoo.com>
2022-05-30 17:00:55 -0700

    Add edit page and links

--- app.js
+++ app.js
@@ -37,12 +37,17 @@ app.get("/campgrounds/new", (req, res) => {
 app.post("/campgrounds", async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`)
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
 });
 
 app.listen(3000, () => {
