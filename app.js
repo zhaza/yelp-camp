23dc362cb8b907ba37ea312c6b744c4c72e438f2
zhaza <Khail.ballin12@yahoo.com>
2022-05-30 21:45:22 -0700

    Add ejs-mate and layout to all pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const ejsMate = require('ejs-mate');
 const methodOverride = require("method-override");
 const res = require("express/lib/response");
 
@@ -21,6 +22,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
